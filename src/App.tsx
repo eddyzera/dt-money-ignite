1784e1a35403145d94f5e6cc978dfd316aaa05c8
eddyzera <edgar.nascimento@newgo.com>
2023-01-10 14:53:03 -0300

    feat: created a style to the project and reset css default

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/global'
 
 export const App: React.FunctionComponent = () => {
- return <h1>Hello World</h1>
+ return (
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
+    <h1>Hello World</h1>
+  </ThemeProvider>
+ )
 }
